51c930ee432e43199f44ccad357f9f0eff4cc5f4
Skarzynski76 <grzegorzskarzynski@icloud.com>
2026-07-30 09:28:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jwstudy-v95';
+const CACHE = 'jwstudy-v100';
 // Najważniejszy jest index.html — reszta to dodatki. Każdy plik zapisujemy OSOBNO,
 // żeby brak jednego (np. ikony) nie przerwał całego zapisu offline.
 const CORE = ['./', './index.html'];
